b380806f58a92983ae1b1f8f05d22d5c0290b9ef
chakrapanikotnana <kchakrapani123@gmail.com>
2024-07-05 23:59:55 +0530

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertyComponent } from './property/property.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
